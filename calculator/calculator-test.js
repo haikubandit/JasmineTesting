190dbeab718e70e04fecb4c8a7a01c3251bfe17b
Daniel Knapp <knapparatus@gmail.com>
2021-01-15 15:59:08 -0600

    Cleaned up calculator Jasmine tests + updated Jasmine CDN to 3.6.0

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,11 @@
 it('should calculate the monthly rate correctly', function() {
 	// ...
-	expect(calculateMonthlyPayment()).toBeInstanceOf(String);
 	const values = {
 		amount: 10000,
 		years: 8,
 		rate: 5.8
 	};
+	expect(calculateMonthlyPayment(values)).toBeInstanceOf(String);
 	expect(calculateMonthlyPayment(values)).toEqual('130.44');
 	expect(Math.floor(calculateMonthlyPayment({ amount: 10000, years: 10, rate: 4.5 }))).toEqual(103);
 	expect(Math.floor(calculateMonthlyPayment({ amount: 10000, years: 10, rate: 4.5 }))).toEqual(103);
@@ -13,14 +13,12 @@ it('should calculate the monthly rate correctly', function() {
 
 it('should return a result with 2 decimal places', function() {
 	// ..
+	const values = {
+		amount: 10043,
+		years: 8,
+		rate: 5.8
+	};
+	expect(calculateMonthlyPayment(values)).toEqual('131.00');
 });
 
 /// etc
-// expect(calculateMonthlyPayment(2000)).toEqual(2000);
-// expect(calculateMonthlyPayment(Math.floor({ amount: 20000, years: 30, rate: 0.4 }))).toEqual(666);
-// expect(calculateMonthlyPayment(Math.floor({ amount: 20000, years: 30, rate: 0.4 })).toEqual(666);
-
-// 	expect(Math.floor(calculateMonthlyPayment({ amount: 10000, years: 10, rate: 4.5 }))).toEqual(103),
-// 	expect(calculateMonthlyPayment({ amount: 10000, years: 10, rate: 4.5 })).toEqual(103.64),
-
-// expect(calculateMonthlyPayment({ amount: 10000, years: 10, rate: 4.5 })).toBeInstanceOf(String);
