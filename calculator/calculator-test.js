4a4917289fb7389370612778c86ea3adc26e04d1
Daniel Knapp <knapparatus@gmail.com>
2021-01-15 15:37:18 -0600

    updated jasmine tests

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,26 @@
-
-it('should calculate the monthly rate correctly', function () {
-  // ...
+it('should calculate the monthly rate correctly', function() {
+	// ...
+	expect(calculateMonthlyPayment()).toBeInstanceOf(String);
+	const values = {
+		amount: 10000,
+		years: 8,
+		rate: 5.8
+	};
+	expect(calculateMonthlyPayment(values)).toEqual('130.44');
+	expect(Math.floor(calculateMonthlyPayment({ amount: 10000, years: 10, rate: 4.5 }))).toEqual(103);
+	expect(Math.floor(calculateMonthlyPayment({ amount: 10000, years: 10, rate: 4.5 }))).toEqual(103);
 });
 
-
-it("should return a result with 2 decimal places", function() {
-  // ..
+it('should return a result with 2 decimal places', function() {
+	// ..
 });
 
 /// etc
+// expect(calculateMonthlyPayment(2000)).toEqual(2000);
+// expect(calculateMonthlyPayment(Math.floor({ amount: 20000, years: 30, rate: 0.4 }))).toEqual(666);
+// expect(calculateMonthlyPayment(Math.floor({ amount: 20000, years: 30, rate: 0.4 })).toEqual(666);
+
+// 	expect(Math.floor(calculateMonthlyPayment({ amount: 10000, years: 10, rate: 4.5 }))).toEqual(103),
+// 	expect(calculateMonthlyPayment({ amount: 10000, years: 10, rate: 4.5 })).toEqual(103.64),
+
+// expect(calculateMonthlyPayment({ amount: 10000, years: 10, rate: 4.5 })).toBeInstanceOf(String);
--- calculator/calculator.js
+++ calculator/calculator.js
@@ -21,9 +21,9 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
-	document.getElementById('loan-amount').value = 20000;
-	document.getElementById('loan-years').value = 30;
-	document.getElementById('loan-rate').value = 0.4;
+	document.getElementById('loan-amount').value = 10000;
+	document.getElementById('loan-years').value = 10;
+	document.getElementById('loan-rate').value = 4.5;
 	update();
 }
 
@@ -37,13 +37,17 @@ function update() {
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
-	let monthlyRate;
-	let principle = values.amount;
-	let periodicInterest = values.rate / 12;
-	let payments = values.years * 12;
+	// let monthlyRate;
+	// let principle = values.amount;
+	// let periodicInterest = values.rate / 12;
+	// let payments = values.years * 12;
 
-	monthlyRate = principle * periodicInterest / (1 - (1 + periodicInterest) ** -payments);
-	return (Math.round(monthlyRate * 100) / 100).toFixed(2);
+	// monthlyRate = principle * periodicInterest / (1 - (1 + periodicInterest) ** -payments);
+	// return (Math.round(monthlyRate * 100) / 100).toFixed(2);
+
+	const monthlyRate = values.rate / 100 / 12;
+	const n = Math.floor(values.years * 12);
+	return (monthlyRate * values.amount / (1 - Math.pow(1 + monthlyRate, -n))).toFixed(2);
 }
 
 // Given a string representing the monthly payment value,
