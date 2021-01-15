bd3c708a5e4d5d7492c6e6ed7f76ad582a541a93
Daniel Knapp <knapparatus@gmail.com>
2021-01-15 17:53:55 -0600

    Updating server tests

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -1,17 +1,27 @@
-describe("Servers test (with setup and tear-down)", function() {
-  beforeEach(function () {
-    // initialization logic
-    serverNameInput.value = 'Alice';
-  });
+describe('Servers test (with setup and tear-down)', function() {
+	beforeEach(function() {
+		// initialization logic
+		serverNameInput.value = 'Alice';
+	});
 
-  it('should add a new server to allServers on submitServerInfo()', function () {
-    submitServerInfo();
+	it('should add a new server to allServers on submitServerInfo()', function() {
+		submitServerInfo();
 
-    expect(Object.keys(allServers).length).toEqual(1);
-    expect(allServers['server' + serverId].serverName).toEqual('Alice');
-  });
+		expect(Object.keys(allServers).length).toEqual(1);
+		expect(allServers['server' + serverId].serverName).toEqual('Alice');
+	});
 
-  afterEach(function() {
-    // teardown logic
-  });
+	it('should create table row element and pass to appendTd function with input value', function() {
+		// submitServerInfo();
+		updateServerTable();
+
+		expect(serverTbody.querySelectorAll('tr').length).toEqual(1);
+	});
+
+	afterEach(function() {
+		// teardown logic
+		// serverNameInput.value = '';
+		// allServers = {};
+		// serverTbody.innerHTML = '';
+	});
 });
