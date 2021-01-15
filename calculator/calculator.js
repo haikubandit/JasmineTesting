bee4f6605ce9d3eacc320a1e2d99ae66bb76fe9d
Daniel Knapp <knapparatus@gmail.com>
2021-01-15 14:21:10 -0600

    Calculator is working but formula seems incorrect

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -24,26 +24,29 @@ function setupIntialValues() {
 	document.getElementById('loan-amount').value = 20000;
 	document.getElementById('loan-years').value = 30;
 	document.getElementById('loan-rate').value = 0.4;
-	// return getCurrentUIValues();
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
-	console.log(calculateMonthlyPayment(getCurrentUIValues()));
+	updateMonthly(calculateMonthlyPayment(getCurrentUIValues()));
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+	let monthlyRate;
 	let principle = values.amount;
 	let periodicInterest = values.rate / 12;
 	let payments = values.years * 12;
 
-	return principle * periodicInterest / (1 - (1 + periodicInterest) ** -payments);
+	monthlyRate = principle * periodicInterest / (1 - (1 + periodicInterest) ** -payments);
+	return (Math.round(monthlyRate * 100) / 100).toFixed(2);
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
-function updateMonthly(monthly) {}
+function updateMonthly(monthly) {
+	document.getElementById('monthly-payment').innerHTML = monthly;
+}
