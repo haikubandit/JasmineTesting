035e37166a80d8255d8bc5400383e7b3e3ab083a
Daniel Knapp <knapparatus@gmail.com>
2021-01-15 18:52:43 -0600

    Initialize Payment and Helper test code

---
+++ tip-pool/helpers.test.js
@@ -0,0 +1,13 @@
+describe('Helper function tests (with setup and tear-down)', function() {
+	beforeEach(function() {
+		// initialization logic
+	});
+
+	it('should sum total for various types on sumPaymentTotal(type)', function() {
+		//...
+	});
+
+	afterEach(function() {
+		// teardown logic
+	});
+});
---
+++ tip-pool/payments.test.js
@@ -0,0 +1,13 @@
+describe('Payments test (with setup and tear-down)', function() {
+	beforeEach(function() {
+		// initialization logic
+	});
+
+	it('should add a new payment to allPayments on submitPaymentInfo()', function() {
+		//...
+	});
+
+	afterEach(function() {
+		// teardown logic
+	});
+});
